718760c5dbe750099630d4ec9fd4a11c27aa251d
Telechi Nicolae <tux2nicolae@gmail.com>
2021-11-06 13:01:43 +0200

    Insert data into the databse

--- index.js
+++ index.js
@@ -11,20 +11,23 @@ async function main() {
     console.log({yelpData});
 
     db.serialize(function() {
+        db.run("CREATE TABLE googleRestaurants (name TEXT, phone TEXT, latitude REAL, longitude REAL)");
+        db.run("CREATE TABLE yelpRestaurants (name TEXT, phone TEXT, latitude REAL, longitude REAL)");
+        db.run("CREATE TABLE restaurantMatches (googleRestaurantId INTEGER NOT NULL, yelpRestaurantId INTEGER NOT NULL, certainty TEXT CHECK(certainty IN ('Highest','High','Medium')))");
 
-    db.run("CREATE TABLE googleRestaurants (name TEXT, phone TEXT, latitude REAL, longitude REAL)");
-    db.run("CREATE TABLE yelpRestaurants (name TEXT, phone TEXT, latitude REAL, longitude REAL)");
-    db.run("CREATE TABLE restaurantMatches (googleRestaurantId INTEGER NOT NULL, yelpRestaurantId INTEGER NOT NULL, certainty TEXT CHECK(certainty IN ('Highest','High','Medium')))");
+        // insert google data
+        const googleStmt = db.prepare("INSERT INTO googleRestaurants(name, phone, latitude, longitude) VALUES (?, ?, ?, ?)");
+        for (const restaurant of googleData) {
+            googleStmt.run([restaurant.name, restaurant.phone, restaurant.latitude, restaurant.longitude]);
+        }    
+        googleStmt.finalize();
 
-    //   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    //   for (var i = 0; i < 10; i++) {
-    //       stmt.run("Ipsum " + i);
-    //   }
-    //   stmt.finalize();
-
-    //   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    //       console.log(row.id + ": " + row.info);
-    //   });
+        // insert yelp data
+        const yelpStmt = db.prepare("INSERT INTO yelpRestaurants(name, phone, latitude, longitude) VALUES (?, ?, ?, ?)");
+        for (const restaurant of googleData) {
+            yelpStmt.run([restaurant.name, restaurant.phone, restaurant.latitude, restaurant.longitude]);
+        }    
+        yelpStmt.finalize();
     });
 
     db.close();
